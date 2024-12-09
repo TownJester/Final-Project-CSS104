49e29d273a242f05ea7d61913b65b4b6d91b5ac5
Madalyn <madalynshea@icloud.com>
2024-12-09 14:37:27 -0700

    Add files via upload

---
+++ FinalScript.js
@@ -0,0 +1,22 @@
+//function is to validate the password
+function ValForm(CPass){
+
+    // var SLogs is to store the entered user password
+    var SLogs = document.getElementById("LogPass").value;
+    //I need to make a variable for a right password
+    var CPass
+    //Then i need to check to make sure they entered a password
+    //Then it's about checking for the right password
+    //SLogs and CPass has to == then
+    //This is a headahce
+    }
+
+
+
+
+}
+
+
+
+// Look at references please
+// code the form functions and whatever else i need
