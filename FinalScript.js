9790fe4b7324262414a31799ec8a17f3f21e9e30
Madalyn <madalynshea@icloud.com>
2024-12-10 11:43:14 -0700

    Add files via upload

--- FinalScript.js
+++ FinalScript.js
@@ -1,20 +1,27 @@
 //function is to validate the password
-function ValForm(CPass){
+function ValP(){
 
     // var SLogs is to store the entered user password
-    var SLogs = document.getElementById("LogPass").value;
+    var UserL1 = document.getElementById("User1").value;
+    var InLog = document.getElementById("LogPass").value;
+    //i need to make this more secure
+    if (UserL1 === "CyrusCox" && InLog === "Secret"){
+        location.reload("Week 8 SEO.html")
+        alert("Hi");
+    }
+    else{
+        alert("WRONG");
+        return;
+    }
+
+    //I'm like somewhat there??
     //I need to make a variable for a right password
-    var CPass
     //Then i need to check to make sure they entered a password
     //Then it's about checking for the right password
     //SLogs and CPass has to == then
     //This is a headahce
     }
 
-
-
-
-}
 
 
 
